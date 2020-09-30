4fe97a0e89926a7c6c8fdda06d603735a05875c9
juanf.perez.2017@upb.edu.co <juanf.perez.2017@upb.edu.co>
2020-09-30 00:16:31 -0500

    Updates

---
+++ precache-manifest.e5d5f359301ca152dde358bd0297919b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7282764f2f4a1211fbbec7993ede153",
+    "url": "/encuesta/index.html"
+  },
+  {
+    "revision": "53f540bc257d4cbe44d5",
+    "url": "/encuesta/static/css/main.53630c35.chunk.css"
+  },
+  {
+    "revision": "80ecd6e0a1f20f275f87",
+    "url": "/encuesta/static/js/2.59baa216.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/encuesta/static/js/2.59baa216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f540bc257d4cbe44d5",
+    "url": "/encuesta/static/js/main.8dbae67c.chunk.js"
+  },
+  {
+    "revision": "57cdb3d257fd59b991d4",
+    "url": "/encuesta/static/js/runtime-main.a41aeba5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/encuesta/static/media/logo.5d5d9eef.svg"
+  }
+]);
